5257b5cd6e5815dd3cca955b3347e0b49f0a411a
Claudia Torreiro <143898254+claudiatorreiro@users.noreply.github.com>
2025-08-19 23:53:08 +0000

    comandos finalizar no git

---
+++ comandos_git.py
@@ -0,0 +1,9 @@
+comandos = ("git status", "git add .", "git commit -m ()", "git push")
+print(comandos)
+
+### comandos para finalizar
+
+# git status
+# git add .
+# git commit -m "descrição"
+# git push
